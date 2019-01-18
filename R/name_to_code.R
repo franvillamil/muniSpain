75aa2f367ab51ab195c4658dae723ce37eb76b97
Fran Villamil <fvillami@icr-m07.local>
2019-01-18 12:54:35 +0100

    fix bugs in name_to_code

--- R/name_to_code.R
+++ R/name_to_code.R
@@ -1,7 +1,11 @@
 #' Returns INE codes corresponding to municipality names.
 #'
 #' @param muni Character string or character vector. It returns a character vector of
-#'   INE codes equivalent to the input. If not found, it returns NA.
+#'   INE codes equivalent to the input. If not found, it returns NA. In case two codes
+#'   share a municipality name in the same province, it returns the largest code (usually,
+#'   the latest municipality) and a warning. Usually, this is due to a municipality changing
+#'   name and code, and then returning to the old denomination. A more problematic case is
+#'   where two different municipalities in the same province share a name in the past.
 #' @param prov Optional, character vector. Include in case of name duplicates.
 #'   If a duplicate is found and no province was specified, it returns an error message.
 #'
@@ -33,38 +37,57 @@ name_to_code = function(muni, prov = NULL){
   # Adapt muni to lower-case and add regex
   muni_ptt = paste0("(^|;)", muni, "(;|$)")
 
-  # If something in prov, check
+  # Functions to find rows in codelist
+  return_rows_noprov = function(muni_regex){
+    rows = codelist[grepl(muni_regex, codelist$names, ignore.case = TRUE),]
+    if(length(unique(rows$prov)) > 1){
+      stop("Identical municipality names in different provinces. Please specify provinces.")
+    } else if(nrow(rows)>1 & length(unique(rows$prov))==1){
+      print(paste0("Identical municipality names within the same province, for '",
+        gsub("\\(|\\)|\\$|\\;|\\^|\\|", "", muni_regex),
+        "'. Returning highest code: ", rows$muni_code[rows$muni_code==max(rows$muni_code)]))
+    }
+    return(rows)
+  }
+
+  return_rows = function(muni_regex, prov){
+    rows = codelist[grepl(muni_regex, codelist$names, ignore.case = TRUE) &
+      codelist$prov_name == tolower(prov),]
+    if(nrow(rows) > 1){
+      print(paste0("Identical municipality names within the same province, for '",
+        gsub("\\(|\\)|\\$|\\;|\\^|\\|", "", muni_regex), "' in ", prov,
+        ". Returning highest code: ", rows$muni_code[rows$muni_code==max(rows$muni_code)]))
+    }
+    return(rows)
+  }
+
+  # If something in prov, check & apply relevant variable
   if(!is.null(prov)){
+
     if(length(prov) != length(muni)){
       stop("prov and muni must have the same length")
-    }
-    if(!all(tolower(prov) %in% codelist$prov_name)){
+    } else if(!all(tolower(prov) %in% codelist$prov_name)){
       stop("Province names not valid - misspelled?")
     }
+    rows_found = apply(cbind(muni_ptt, prov), 1, function(x) return_rows(x[1], x[2]))
 
-    mp = data.frame(p = prov, m = muni_ptt, i = NA)
-    for(i in 1:nrow(mp)){
-      found = which(grepl(mp[i, "p"], codelist$prov_name, ignore.case = TRUE) &
-        grepl(mp[i, "m"], codelist$names, ignore.case = TRUE))
-      mp[i, "i"] = found
-    }
-    found_codes = codelist$muni_code[mp$i]
-    return(found_codes)
-
+  # If prov = NULL
   } else {
-
-    found = sapply(muni_ptt, function(x){
-      result = grep(x, codelist$names, ignore.case = TRUE)
-      if(length(result) == 0){result = NA}
-      result})
-
-    if(class(found) == "list" | length(found) > length(muni_ptt)){
-      stop("Duplicates found - please specify provinces")
-    } else {
-      found_codes = codelist$muni_code[found]
-      return(found_codes)
-    }
-
+    rows_found = lapply(muni_ptt, function(x) return_rows_noprov(x))
   }
+
+  # Get municipality codes
+  rows_to_code = function(codelist_row){
+    code = codelist_row$muni_code
+    if(length(code) == 0){
+      code = NA
+    } else if(length(code) > 1){
+      code = code[code == max(code)]
+    }
+    return(code)
+  }
+
+  codes = unlist(lapply(rows_found, rows_to_code))
+  return(codes)
 
 }
